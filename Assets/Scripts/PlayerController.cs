b81970806ff95095cbf3f8b2e337551fd068e6bb
Christopher Soriano <2832@holbertonschool.com>
2021-12-23 13:01:19 -0400

    add win and lose conditions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,9 +11,14 @@ public class PlayerController : MonoBehaviour
     public int health = 5;
 
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
     }
 
     // Update is called once per frame
@@ -48,6 +54,10 @@ public class PlayerController : MonoBehaviour
         {
             health--;
             Debug.Log($"Health: {health}");
+        }
+        else if (other.tag == "Goal")
+        {
+            Debug.Log("You win!");
         }
     }
 }
